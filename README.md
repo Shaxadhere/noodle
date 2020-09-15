f83dcc08b2a0c7c8dd36a2143a4ecb1650925130
Shehzad <shaxad.here@gmail.com>
2020-09-15 11:21:55 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Phindex
+=======
+
+[![Scc Count Badge](https://sloc.xyz/github/boyter/Phindex/)](https://github.com/boyter/Phindex/)
+
+This is the the resulting code of a 5 part series which you can read here http://www.boyter.org/2013/01/code-for-a-search-engine-in-php-part-1/
+
+For help setting it up see the following link http://www.boyter.org/2014/04/installing-phindex/ which walks through getting setup with 10,000 documents from scatch.
+
+Keep in mind this was never intended to be a drop in replacement for any existing search service or even a install and index solution. You will need to modify code to do anything with this. This may change in the future as I have some additional code which should allow the creation of a full search solution.
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Google Homepage</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="assets/style.css" rel="stylesheet">
+
+    
+  </head>
+  
+  <body>
+    <header>
+      <nav>
+        <ul id="nav_bar">
+          <li class="nav-links"><a href="#">xsa</a></li>
+          <li id="sign_in"><a href="#">Github</a></li>
+        </ul>  
+      </nav>  
+    </header>  
+    
+    <!-- GOOGLE IMG -->  
+    <div class="google">
+      <a href="#" id="google_logo"><img style="width:320px;" src="assets/logo.png" alt=" photo google-logo_zpspkcztsjo.png"/></a>
+    </div>
+    
+    <!-- FORM SEARCH -->  
+    <div class="form">  
+      <form>
+        <label for="form-search"></label>
+        <div class="col-xs-offset-3 col-xs-6 col-md-offset-4 col-md-4">
+            <input type="email" class="form-control" name="email" placeholder="email">
+        </div>
+      </form>
+    </div>  
+    
+    <!-- BUTTONS -->
+    <div class= "buttons">  
+      <input type="submit" class="btn btn-primary" value="Lookup" id="google_search">
+      <input type="submit" class="btn btn-default" value="About Lookup" id="im_feeling_lucky">
+    </div>
+      
+    <!-- FOOTER -->
+    <footer>
+        <ul class="footer-left">
+          <li><a href="#">Advertising</a></li>
+          <li><a href="#">Business</a></li>
+          <li><a href="#">About</a></li> 
+        </ul>
+        <ul class="footer-right">    
+          <li><a href="#">Privacy</a></li>
+          <li><a href="#">Terms</a></li>
+          <li><a href="#">Settings</a></li>
+        </ul>       
+    </footer>   
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>   
+  </body>
+</html>
